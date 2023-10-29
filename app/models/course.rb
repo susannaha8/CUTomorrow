e3c3c5c6e389f8073117d2b7af42a4ea5f987c5a
susannaha8 <66691218+susannaha8@users.noreply.github.com>
2023-10-28 22:39:02 -0400

    adjusted name in courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,9 @@ class Course < ActiveRecord::Base
 	def self.get_courses_by_requirement(reqID)
 		Course.joins(:coursereq).where(coursereqs: {reqID: reqID})
 	end
+
+	#maybe
+	#def self.check_if_course_fulfills_req(courseID, reqID)
+
+	#end
 end

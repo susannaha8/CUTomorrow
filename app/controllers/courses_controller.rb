52b9edba1feb8d8f07c5975a872ff3920adf477d
claudiflower <liharcl@gmail.com>
2023-10-30 19:08:27 -0400

    Edits to index and controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,8 +1,10 @@
 class CoursesController < ApplicationController
   def index
     # want to display the Major, Requirements, and Classes in the view
-    # @major = 
-    # @requirements = filter requirements for specific majorminor
-    # @schedule = Schedule.find(params[:uni]) filter Schedules by uni?
+    @major = 1 #will need to get this from profile
+    @uni = "sma2243" #will need to get this from profile
+    @major_name = "Computer Science"
+    @requirements = Requirement.get_requirements_by_major(@major)
+    @schedule = Schedule.find(params[:uni])
   end
 end

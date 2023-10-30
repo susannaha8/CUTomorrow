7ccfb27a864500d095ff8153f12ac05b462442fb
claudiflower <liharcl@gmail.com>
2023-10-30 03:11:21 -0400

    Added another table to feature view with required courses, which you can select add class

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
   def index
     # want to display the Major, Requirements, and Classes in the view
     # @major = 
-    # @requirements =
+    # @requirements = filter requirements for specific majorminor
     # @schedule = Schedule.find(params[:uni]) filter Schedules by uni?
   end
 end

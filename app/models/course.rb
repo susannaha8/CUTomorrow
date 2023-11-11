8e7e4f47a2547fd2320f5cf694d2efe7e530686d
susannaha8 <66691218+susannaha8@users.noreply.github.com>
2023-11-11 09:55:32 -0500

    fixed association

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-	belongs_to :coursereq, :foreign_key => "courseID"
+	has_many :coursereq, :foreign_key => "courseID"
 	has_many :schedule, :foreign_key => "courseID"
 	def self.get_courses_by_requirement(reqID)
 		Course.joins(:coursereq).where(coursereqs: {reqID: reqID})
--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,4 +1,5 @@
 class Requirement < ActiveRecord::Base
+	has_many :coursereq, :foreign_key => "reqID"
 	has_many :schedule, :foreign_key => "reqID"
 	def self.get_requirements_by_major(major)
 		Requirement.where(major_minorID: major)

275cee5baef507ba8aeb032e90d1fee1611d6872
susannaha8 <66691218+susannaha8@users.noreply.github.com>
2023-11-13 14:46:53 -0500

    able to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,16 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-	  flash[:notice] = (params[:email]).to_s
+	  #flash[:notice] = " Email: #{params[:email]}"
 	  @student = Student.find_by(email: params[:email])
+	  @id = @student.id
+	  @password = params[:password]
       
-	  if @student && @student.authenticate(params[:email][:password])
+	  if @student && @student.authenticate(@password)
 	    session[:student_id] = @student.id
 	    redirect_to schedule_path
 	  else
-	    #flash[:notice] = "Login failed"
+	    flash[:notice] = "Login failed"
 	    redirect_to login_path
 	  end
 	end

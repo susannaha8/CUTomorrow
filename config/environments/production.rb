cc371636a9f7fc90c740affea5c7271819e6ca8d
susannaha8 <66691218+susannaha8@users.noreply.github.com>
2023-11-15 17:59:28 -0500

    log info command

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ CUTomorrow::Application.configure do
   config.action_controller.perform_caching = true
 
   config.eager_load = true
-
+  config.log_level = :info
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
